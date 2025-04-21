3fbd6404664045e3d9e656b8242c84fdee6f52ca
sledrabbit <20589051+sledrabbit@users.noreply.github.com>
2025-04-21 00:37:02 -0700

    vapor-server linting/formatting

--- backend/swift/vapor-server/Package.swift
+++ backend/swift/vapor-server/Package.swift
@@ -4,29 +4,29 @@
 import PackageDescription
 
 let package = Package(
-    name: "vapor-server",
-    platforms: [
-        .macOS(.v10_15)
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.6.0"),
-        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.7.0"),
-        .package(url: "https://github.com/swift-server/swift-openapi-vapor", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/vapor", from: "4.89.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "vapor-server",
-            dependencies: [
-                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
-                .product(name: "OpenAPIVapor", package: "swift-openapi-vapor"),
-                .product(name: "Vapor", package: "vapor"),
-            ],
-            plugins: [
-                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")
-            ]
-        )
-    ]
+  name: "vapor-server",
+  platforms: [
+    .macOS(.v10_15)
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.6.0"),
+    .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.7.0"),
+    .package(url: "https://github.com/swift-server/swift-openapi-vapor", from: "1.0.0"),
+    .package(url: "https://github.com/vapor/vapor", from: "4.89.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "vapor-server",
+      dependencies: [
+        .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+        .product(name: "OpenAPIVapor", package: "swift-openapi-vapor"),
+        .product(name: "Vapor", package: "vapor"),
+      ],
+      plugins: [
+        .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")
+      ]
+    )
+  ]
 )
