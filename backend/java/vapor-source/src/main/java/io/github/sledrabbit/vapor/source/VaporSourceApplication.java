ab8035a7fa4cfb6bbd117fe14d604b76f6c8bb09
sledrabbit <20589051+sledrabbit@users.noreply.github.com>
2025-10-19 17:27:46 -0700

    updated README for the porting of Swift server/lambda to Java and Go versions

---
+++ backend/java/vapor-source/src/main/java/io/github/sledrabbit/vapor/source/VaporSourceApplication.java
@@ -0,0 +1,13 @@
+package io.github.sledrabbit.vapor.source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaporSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaporSourceApplication.class, args);
+	}
+
+}
---
+++ backend/java/vapor-source/src/test/java/io/github/sledrabbit/vapor/source/VaporSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sledrabbit.vapor.source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaporSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
