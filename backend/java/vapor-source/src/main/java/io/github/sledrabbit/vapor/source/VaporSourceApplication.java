0af9a626a5b9b1a6997f9ef5ecf19308d6157b28
sledrabbit <20589051+sledrabbit@users.noreply.github.com>
2025-10-29 14:07:02 -0700

    server 'hello, world!' up and running

--- backend/java/vapor-source/src/main/java/io/github/sledrabbit/vapor/source/VaporSourceApplication.java
+++ backend/java/vapor-source/src/main/java/io/github/sledrabbit/vapor/source/VaporSourceApplication.java
@@ -2,11 +2,19 @@ package io.github.sledrabbit.vapor.source;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class VaporSourceApplication {
 
-	public static void main(String[] args) {
+    @RequestMapping("/")
+    String home() {
+        return "Hello, world!";
+    }
+
+	void main(String[] args) {
 		SpringApplication.run(VaporSourceApplication.class, args);
 	}
 
